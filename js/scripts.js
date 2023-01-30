0073b12831573e0b9475e9f40984da18588ead2b
Bruno D'Ambrosio <100494971+Br1dam@users.noreply.github.com>
2023-01-30 15:36:09 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@ const dark_light = document.querySelector("#dark-light");
 
 const darkModeon = () =>{
     document.body.classList.add("dark");
+    // Agrego la clase dark, cambiando las variables definidas en :root
+
     localStorage.setItem("darkMode","on");
 }
 
 const darkModeoff = () =>{
     document.body.classList.remove("dark");
+    //Quito la clase dark, alternando entre los dos estilos
     localStorage.setItem("darkMode","off");
 }
+
+//Si estaba activado, al recargar la pag se mantiene
 
 if(darkMode === "on"){
     darkModeon();
@@ -17,6 +22,7 @@ if(darkMode === "on"){
 
 
 dark_light.addEventListener("click", () => {
+    //Busco en el LocalStorage el valor actual de darkMode
     darkMode = localStorage.getItem('darkMode'); 
     if (darkMode !== "on") {
         darkModeon();
